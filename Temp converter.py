c4aa492cbe0bab5982c31b41f607b9eadd449f9d
Soumyojyoti Sen <82030266+iSoumyo@users.noreply.github.com>
2021-06-09 19:33:58 +0530

    Add files via upload

---
+++ Temp converter.py
@@ -0,0 +1,6 @@
+t = float(input('Enter todays Temperature:'))
+c = (t - 32) * 5/9
+print('According to my calculation todays Celsius: {0}'.format(c))
+
+f = (t * 9/5) + 32
+print('According to my calculation todays Farenhite: {0}'.format(f))
